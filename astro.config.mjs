c71c6bbbe4dd69db815ddf4689de82cf61fc4007
marcxzo <alesna.business@gmail.com>
2026-06-11 15:22:33 -0500

    added preact dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://pracstro.netlify.app/"
+  site: "https://pracstro.netlify.app/",
+  integrations: [preact()]
 });
